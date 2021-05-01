ec67f0406cbf7719f94b287cb321b745eef79579
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-05-02 01:20:20 +0200

    add gif model plus migrations

--- apps/gifs/models.py
+++ apps/gifs/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
-# Create your models here.
+
+GIF_STATUS = (
+    ('P', 'pending'),
+    ('A', 'accepted'),
+    ('R', 'rejected'),
+)
+
+class Gif(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.CharField(max_length=1, choices=GIF_STATUS, default='P')
+    uid = models.CharField(max_length=32, blank=True)
+    gif = models.ImageField(upload_to='gifs/%Y/%m')
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['uid'], name='unique-uid')]
+        
+    def save(self, *args, **kwargs):
+        if not self.uid:
+            self.uid = get_random_string(length=32)
+            self._rename()
+        super().save(*args, **kwargs) 
+
+    def _rename(self):
+        self.gif.name = f"{self.uid}.gif"
