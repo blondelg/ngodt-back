386e5790fca121363e3af813d42fff17c22a119d
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-05-02 01:20:42 +0200

    add gif model to adminfor test purposes

--- apps/gifs/admin.py
+++ apps/gifs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Gif
 
 # Register your models here.
+@admin.register(Gif)
+class GifAdmin(admin.ModelAdmin):
+  list_display = [field.name for field in Gif._meta.get_fields()]
