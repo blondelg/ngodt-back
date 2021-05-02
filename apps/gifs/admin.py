88666f18f73d53962074189b01be42a09e5d5494
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-05-02 16:17:15 +0200

    work on models

--- apps/gifs/admin.py
+++ apps/gifs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Gif
 
-# Register your models here.
 @admin.register(Gif)
 class GifAdmin(admin.ModelAdmin):
-  list_display = [field.name for field in Gif._meta.get_fields()]
+  list_display = ['name', 'status', 'uid', 'gif']
+  readonly_fields = ['uid']
+  filter_horizontal = ['tags']
