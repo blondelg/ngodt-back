8ffd6d59bb9775883dca221b3feb73f16ef67652
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-05-29 19:06:02 +0200

    upgrade jwt auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from environs import Env
 import os
 
+from datetime import timedelta
+
 env = Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,9 +143,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
 }
